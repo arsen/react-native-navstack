c5d7de95007fbf7674465a9b522efd97fffb89ba
Arsen Ghazaryan <arsen.ghazaryan@gmail.com>
2017-01-09 02:42:13 -0800

    add PushFromTop transition

---
+++ src/transitions/PushFromTop.js
@@ -0,0 +1,17 @@
+import { LayoutAnimation } from 'react-native';
+
+export default {
+  animation: {
+    duration: 250,
+    update: {
+      type: LayoutAnimation.Types.easeIn,
+      springDamping: 0.7,
+    },
+  },
+  screen1: {
+    top: 0.3,
+  },
+  screen2: {
+    top: -1,
+  },
+};

6251c137280e26d661ad10d79fb4ead1d79d07bf
Arsen Ghazaryan <arsen.ghazaryan@gmail.com>
2017-01-09 02:43:03 -0800

    adjust SlideFromRight transition

--- src/transitions/SlideFromRight.js
+++ src/transitions/SlideFromRight.js
@@ -3,24 +3,13 @@ import { LayoutAnimation } from 'react-native';
 export default {
   animation: {
     duration: 400,
-    // create: {
-    //   type: LayoutAnimation.Types.spring,
-    //   property: LayoutAnimation.Properties.opacity,
-    //   springDamping: 0.7,
-    // },
-    // delete: {
-    //   type: LayoutAnimation.Types.spring,
-    //   property: LayoutAnimation.Properties.opacity,
-    //   springDamping: 0.7,
-    // },
     update: {
       type: LayoutAnimation.Types.spring,
       springDamping: 0.7,
     },
   },
   screen1: {
-    left: -0.5,
-    opacity: 1,
+    left: 0 ,
   },
   screen2: {
     left: 1,
